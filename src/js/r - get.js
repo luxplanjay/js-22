c3ffd09a214073b019420d6170e1f37fae73ae30
DenisOrlenko <ghntrdo@gmail.com>
2023-02-17 23:15:00 +0300

    js

--- src/js/r - get.js
+++ src/js/r - get.js
@@ -1,13 +1,29 @@
+// 1- Создаю запрос на мой СЕРВЕР-БОЛВАНКУ
+// fetch('http://localhost:4040/books/7')
+//   .then(response => response.json())
+//   .then(console.log);
+  //
+  //
+// 2-
+// Базовый АДРЕС
+// SCREAMING_CASE - ЗАГЛАВНЫМИ БУКВАМИ
 const BASE_URL = 'http://localhost:4040';
-
+// /books - СЕРВЕР в ОТВЕТЕ возвращает вcю КОЛЛЕКЦИЮ ресурсов
+// bookId - СЕРВЕР в ОТВЕТЕ возвращает ОПРЕДЕЛЕННЫЙ РЕСУРС по ID
+//
+// Создаю запрос на весь ресурс через ф-ю fetchBooks()
 function fetchBooks() {
   return fetch(`${BASE_URL}/books`).then(res => res.json());
 }
-
+//
+// Создаю запрос на ресурс по ID через ф-ю fetchBookById(bookId)
 function fetchBookById(bookId) {
   return fetch(`${BASE_URL}/books/${bookId}`).then(res => res.json());
 }
 
-fetchBooks();
-fetchBookById(2);
-fetchBookById(4);
+// ВЫЗЫВАЮ ф-ю
+fetchBooks()
+fetchBookById(4)
+fetchBookById(2)
+
+// 3- Перрехожу в файл [с-post.js]
--- src/js/u - patch.js
+++ src/js/u - patch.js
@@ -1,5 +1,37 @@
+// 1- ОБНОВЛЕНИЕ - укзываю уник. идентификатор ресурса, который хочу обновить (по ID - fetch(`.....books/${bookId})
+// Метод PUT - полностью перезаписывает св-ва обьекта(полная замена) =>
+// => при отправлении запроса об обновлении на СЕРВЕР(PUT) и указании хотя бы одного измененного св-ва из обьекта
+// => происходит полная замена указанного ресурса
+//
+// Метод PATCH - может изменять отдельные св-ва обьекта (т.е. ресурсов)
+//
+// 2- PATCH =
+// 2.1- указываю обьект(ресурс), в котором хочу обновить св-во (по ID-(...books/ID) => fetch(`.....books/${bookId} или books/15)
+// 2.2- указываю метод для обновления - method: 'PATCH'
+// 2.3- указываю св-во, которое хочу обновить в обьекте => JSON.stringify({title: 'Новая тестовая книга по HTML'})
+const options = {
+  // options = параметры запросы
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ title: 'Новая тестовая книга по HTML' }),
+};
+
+// отправляю запрос - fetch() => если получаю успешный ответ - то выполняется метод .then() -
+// => внутри которого вызывается ф-я с параметром res - аргументом для этого параметра служит ответ (данные), который пришел с БЕКЕНДА
+fetch(`http://localhost:4040/books/34`, options).then(res =>
+  res.json().then(console.log),
+);
+
+// ==========================================
+// 3- Переношу fetch() в ф-ю
 const BASE_URL = 'http://localhost:4040';
 
+// update - указываю обьект со св-ми, которые хочу обновить
+// bookId - указываю ID обьекта, в котором хочу обновить св-ва
+// body: JSON.stringify(update) - перевожу в формат JSON св-ва, которые хочу обновить
+// return fetch() - возвращаю во из ф-ии во внешний код - рез-т метода fetch() => т.е. рез-т метода then() - return РАСПАРСЕННЫЙ ПРОМИС,
 function updateBookById(update, bookId) {
   const options = {
     method: 'PATCH',
@@ -12,8 +44,8 @@ function updateBookById(update, bookId) {
   return fetch(`${BASE_URL}/books/${bookId}`, options).then(res => res.json());
 }
 
-// updateBookById({ title: 'Большая новая книга по NODEJS' }, 19);
+updateBookById({ title: 'Большая новая книга по NODEJS' }, 19);
 
-// updateBookById({ rating: 1 }, 18);
+updateBookById({ rating: 1 }, 18);
 
-// updateBookById({ rating: 4, author: 'Манго' }, 17);
+updateBookById({ rating: 4, author: 'Манго' }, 17);
