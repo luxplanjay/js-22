3563e7cbb0f874e246988068ff324c094d2ea290
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2023-03-14 12:52:51 +0100

    test

--- js/01-switch.js
+++ js/01-switch.js
@@ -101,27 +101,27 @@
  */
 
 //  1. сделать переменные
-const option = 1526;
-let message = '';
+// const option = 1526;
+// let message = '';
 
-// 2. сделать switch 1 2 3
-// 3. в каждом кейсе записать в message строку
-switch (option) {
-  case 1:
-    message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе';
-    break;
+// // 2. сделать switch 1 2 3
+// // 3. в каждом кейсе записать в message строку
+// switch (option) {
+//   case 1:
+//     message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе';
+//     break;
 
-  case 2:
-    message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
-    break;
+//   case 2:
+//     message = 'Курьер доставит заказ завтра с 9:00 до 18:00';
+//     break;
 
-  case 3:
-    message = 'Посылка будет отправлена сегодня';
-    break;
+//   case 3:
+//     message = 'Посылка будет отправлена сегодня';
+//     break;
 
-  default:
-    message = 'Вам перезвонит менеджер';
-}
+//   default:
+//     message = 'Вам перезвонит менеджер';
+// }
 
-// 4. сделать лог message
-console.log(message);
+// // 4. сделать лог message
+// console.log(message);
